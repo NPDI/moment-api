f3014dd8a35d933639819cc76a5ca83aabeb2902
diegodifreitas <diegodifreitas@gmail.com>
2017-08-29 00:42:05 -0300

    Added image routes

--- server/api/routes/routes.ts
+++ server/api/routes/routes.ts
@@ -1,6 +1,7 @@
 import { Application, Request, Response } from 'express';
 
 import TokenRoutes from '../../modules/auth/auth';
+import ImageRoutes from '../../modules/Image/routes';
 import UserRoutes from '../../modules/User/routes';
 
 class Routes {
@@ -11,6 +12,13 @@ class Routes {
     app.route('/api/users/create').all(auth.config().authenticate()).post(UserRoutes.create);
     app.route('/api/users/:id/update').all(auth.config().authenticate()).put(UserRoutes.update);
     app.route('/api/users/:id/destroy').all(auth.config().authenticate()).delete(UserRoutes.destroy);
+
+    app.route('/api/image/all').all(auth.config().authenticate()).get(ImageRoutes.index);
+    app.route('/api/image/:id').all(auth.config().authenticate()).get(ImageRoutes.findOne);
+    app.route('/api/image/create').all(auth.config().authenticate()).post(ImageRoutes.create);
+    app.route('/api/image/:id/update').all(auth.config().authenticate()).put(ImageRoutes.update);
+    app.route('/api/image/:id/destroy').all(auth.config().authenticate()).delete(ImageRoutes.destroy);
+
     app.route('/token').post(TokenRoutes.auth);
 
   }
