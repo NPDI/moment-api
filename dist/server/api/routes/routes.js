dae5e23906ca66fadd3b716471e90327bdc858d0
diegodifreitas <diegodifreitas@gmail.com>
2017-08-31 18:14:28 -0300

    Added new dist files

--- dist/server/api/routes/routes.js
+++ dist/server/api/routes/routes.js
@@ -10,47 +10,36 @@ var Routes = (function () {
     Routes.prototype.initRoutes = function (app, auth) {
         app
             .route("/api/users/all")
-            .all(auth.config().authenticate())
             .get(routes_2.default.index);
         app
             .route("/api/users/:id")
-            .all(auth.config().authenticate())
             .get(routes_2.default.findOne);
         app
             .route("/api/users/create")
-            .all(auth.config().authenticate())
             .post(routes_2.default.create);
         app
             .route("/api/users/:id/update")
-            .all(auth.config().authenticate())
             .put(routes_2.default.update);
         app
             .route("/api/users/:id/destroy")
-            .all(auth.config().authenticate())
             .delete(routes_2.default.destroy);
         app
             .route("/api/images/all")
-            .all(auth.config().authenticate())
             .get(routes_1.default.index);
         app
             .route("/api/images/:id")
-            .all(auth.config().authenticate())
             .get(routes_1.default.findOne);
         app
             .route("/api/images/create")
-            .all(auth.config().authenticate())
             .post(routes_1.default.create);
         app
             .route("/api/images/:id/update")
-            .all(auth.config().authenticate())
             .put(routes_1.default.update);
         app
             .route("/api/images/:id/destroy")
-            .all(auth.config().authenticate())
             .delete(routes_1.default.destroy);
         app
             .route("/upload")
-            .all(auth.config().authenticate())
             .post(routes_1.default.upload);
         app.route("/token").post(auth_1.default.auth);
     };
