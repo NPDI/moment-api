99cadb7b5b4ef0d9ac9500719fb8d8ec7648a344
diegodifreitas <diegodifreitas@gmail.com>
2017-08-31 18:15:12 -0300

    Comented line authenticate in routes

--- server/api/routes/routes.ts
+++ server/api/routes/routes.ts
@@ -10,49 +10,50 @@ class Routes {
   public initRoutes(app: Application, auth: any): void {
     app
       .route("/api/users/all")
-      .all(auth.config().authenticate())
+      /* .all(auth.config().authenticate()) */
       .get(UserRoutes.index);
     app
       .route("/api/users/:id")
-      .all(auth.config().authenticate())
+      /* .all(auth.config().authenticate()) */
       .get(UserRoutes.findOne);
     app
       .route("/api/users/create")
-      .all(auth.config().authenticate())
+      /* .all(auth.config().authenticate()) */
       .post(UserRoutes.create);
+
     app
       .route("/api/users/:id/update")
-      .all(auth.config().authenticate())
+      /* .all(auth.config().authenticate()) */
       .put(UserRoutes.update);
     app
       .route("/api/users/:id/destroy")
-      .all(auth.config().authenticate())
+      /* .all(auth.config().authenticate()) */
       .delete(UserRoutes.destroy);
 
     app
       .route("/api/images/all")
-      .all(auth.config().authenticate())
+      /* .all(auth.config().authenticate()) */
       .get(ImageRoutes.index);
     app
       .route("/api/images/:id")
-      .all(auth.config().authenticate())
+      /* .all(auth.config().authenticate()) */
       .get(ImageRoutes.findOne);
     app
       .route("/api/images/create")
-      .all(auth.config().authenticate())
+      /* .all(auth.config().authenticate()) */
       .post(ImageRoutes.create);
     app
       .route("/api/images/:id/update")
-      .all(auth.config().authenticate())
+      /* .all(auth.config().authenticate()) */
       .put(ImageRoutes.update);
     app
       .route("/api/images/:id/destroy")
-      .all(auth.config().authenticate())
+      /* .all(auth.config().authenticate()) */
       .delete(ImageRoutes.destroy);
 
     app
       .route("/upload")
-      .all(auth.config().authenticate())
+      /* .all(auth.config().authenticate()) */
       .post(ImageRoutes.upload);
 
     app.route("/token").post(TokenRoutes.auth);
