42f353740afbfcc5985dd472da411632c6f4707d
diegodifreitas <diegodifreitas@gmail.com>
2017-09-05 17:25:55 -0300

    Deploy in Heroku

--- server/config/env/production.env.ts
+++ server/config/env/production.env.ts
@@ -3,14 +3,14 @@ import * as path from 'path';
 
 module.exports = {
   env: 'production',
-  db: 'det4547oc9jri',
+  db: process.env.DATABASE_NAME,
   dialect: 'postgres',
   username: 'ppturztezlnzco',
-  password: '18c48962c3f36fab1c12ba5df47379b46f7cb39773694a3d97bc21ca52bc5f64',
+  password: process.env.DATABASE_PASS,
   host: 'localhost',
   serverPort: process.env.PORT,
   pgPort: 5432,
-  dbURL: 'postgres://ppturztezlnzco:18c48962c3f36fab1c12ba5df47379b46f7cb39773694a3d97bc21ca52bc5f64@ec2-107-22-171-11.compute-1.amazonaws.com:5432/det4547oc9jri',
+  dbURL: process.env.DATABASE_URL,
   secret: 'S3cr3t',
   uploadPath: path.join(__dirname, '..', '..', '..', 'upload'),
 };
