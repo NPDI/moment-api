bb6a6b786e17cc78dc9a6ed8828dca389adb08df
diegodifreitas <diegodifreitas@gmail.com>
2017-09-05 15:01:32 -0300

    Added migrations

--- server/config/env/test.env.ts
+++ server/config/env/test.env.ts
@@ -4,14 +4,14 @@ import * as path from 'path';
 
 module.exports = {
   env: 'development',
-  db: 'moment-test',
+  db: 'moment_test',
   dialect: 'postgres',
   username: 'postgres',
   password: 'postgres',
   host: 'localhost',
   serverPort: 3001,
   pgPort: 5432,
-  dbURL: 'postgres://postgres:postgres@localhost:5432/moment-test',
+  dbURL: 'postgres://postgres:postgres@localhost:5432/moment_test',
   secret: 'S3cr3t',
   uploadPath: path.join(__dirname, '..', '..', '..', 'upload'),
 };
---
+++ server/migrations/20170905175159-create-image.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      longitude: {
+        type: Sequelize.STRING
+      },
+      latitude: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Images');
+  }
+};
