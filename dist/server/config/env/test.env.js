f4d73a64679fa610b57cbe81f8c64805cbfdc345
diegodifreitas <diegodifreitas@gmail.com>
2017-09-05 17:01:39 -0300

    Deploy in Heroku

--- dist/server/config/env/test.env.js
+++ dist/server/config/env/test.env.js
@@ -4,14 +4,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var path = require("path");
 module.exports = {
     env: 'development',
-    db: 'moment-test',
+    db: 'moment_test',
     dialect: 'postgres',
     username: 'postgres',
     password: 'postgres',
     host: 'localhost',
     serverPort: 3001,
     pgPort: 5432,
-    dbURL: 'postgres://postgres:postgres@localhost:5432/moment-test',
+    dbURL: 'postgres://postgres:postgres@localhost:5432/moment_test',
     secret: 'S3cr3t',
     uploadPath: path.join(__dirname, '..', '..', '..', 'upload'),
 };
---
+++ dist/server/migrations/20170905181355-create-image.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      longitude: {
+        type: Sequelize.STRING
+      },
+      latitude: {
+        type: Sequelize.STRING
+      },
+      UserId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Images');
+  }
+};
